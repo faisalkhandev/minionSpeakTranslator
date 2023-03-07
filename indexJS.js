8430b36c4b5c97dc559c5a4eeb0c332d57706899
Faisal Khan <faisaldev404@gmail.com>
2023-03-07 14:17:38 +0500

    fixes: fixed some javascript issues

---
+++ indexJS.js
@@ -0,0 +1,12 @@
+let inputTranslate = document.getElementById('translate-input');
+let btnTranslate = document.getElementById('translateBtn');
+let outTranslate = document.getElementById('translate-output');
+
+console.log(btnTranslate)
+
+let getTranslated = () => {
+    console.log("clicked")
+}
+
+btnTranslate.addEventListener("click", getTranslated())
+
