28c612418aeb29243cd188d63fc9c156dc76dbdc
Faisal Khan <faisaldev404@gmail.com>
2023-03-08 15:17:55 +0500

    feature: calling API

--- indexJS.js
+++ indexJS.js
@@ -2,12 +2,31 @@ let inputTranslate = document.getElementById('translate-input');
 let btnTranslate = document.getElementById('translateBtn');
 let outTranslate = document.getElementById('translate-output');
 
+let serverURl = "https://api.funtranslations.com/translate/minion.json"
 
+let getTranslatedURl = (text) => {
+    return serverURl + "?" + "text=" + text;
+}
+
+let handleTheError = (error) => {
+    alert("This API can be called only 5 times an hour, come back after an hour ")
+}
 
 let getTranslated = () => {
 
-    console.log("clicked");
-    outTranslate.innerText = " faisal khan"
+    let inputTxt = inputTranslate.value;
+
+    fetch(getTranslatedURl(inputTxt))
+        .then(response => response.json)
+        .then(json => {
+            let translatedTxt = json.contents.translated;
+            outTranslate.innerText = translatedTxt
+        })
+        .catch(handleTheError)
+
+
+    let
+
 
 }
 
